0019c22dbc50a5abd92cb8582c0be99bbe854e4e
TaehwanGo <gth1123@naver.com>
2021-09-27 22:46:46 +0900

    11.6 ~ 11.7 모듈

---
+++ typescript/10-Javascript/10-3-module2.js
@@ -0,0 +1,7 @@
+// import add, { print as printMessage } from './10-3-module1.js';
+// console.log(add(1, 2));
+// printMessage();
+import * as calculator from './10-3-module1.js';
+console.log(calculator.add(1, 2));
+calculator.print();
+calculator.number;
