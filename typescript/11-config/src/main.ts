c3b2bd6d5a547b2a2c49c46fcc969019d379d7f3
TaehwanGo <gth1123@naver.com>
2021-10-02 17:48:57 +0900

    12.4 디버깅하는 방법 - sourceMap

--- typescript/11-config/src/main.ts
+++ typescript/11-config/src/main.ts
@@ -1,1 +1,13 @@
-console.log('main.ts');
+'use strict';
+
+class Car {
+  engine = 0;
+  move() {
+    const engine = this.engine + 1;
+    console.log('engine bug');
+    console.log(engine);
+  }
+}
+
+const car = new Car();
+car.move();
